2ab0e31b9f7f17b6d3ff80f60d19c4d2511dec73
Richard Sween <sweenr418@gmail.com>
2020-04-06 18:36:55 -0500

    Add in auth and gatsby config files.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,39 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+import React from "react"
+import Layout from "./src/components/Layout"
+import Amplify from "aws-amplify"
+import awsconfig from "./src/aws-exports"
 
-// You can delete this file if you're not using it
+if (typeof window !== `undefined`) {
+  const hostedDomainName = "d1fju52soaxewq.amplifyapp.com/"
+  let redirectUrl = "https://" + hostedDomainName + "/"
+  if (window.location.hostname === "localhost") {
+    redirectUrl = "http://localhost:8000/"
+  } else if (window.location.hostname.endsWith(hostedDomainName)) {
+    redirectUrl = "https://" + window.location.hostname + "/"
+  }
+  const oauth = {
+    domain: "auth.gracelongbeach.com",
+    scope: [
+      "phone",
+      "email",
+      "openid",
+      "profile",
+      "aws.cognito.signin.user.admin",
+    ],
+    redirectSignIn: redirectUrl + "admin/",
+    redirectSignOut: redirectUrl,
+    responseType: "code",
+  }
+  let configUpdate = awsconfig
+  configUpdate.oauth = oauth
+
+  Amplify.configure(configUpdate)
+}
+
+// Wraps every page in a component
+export const wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
