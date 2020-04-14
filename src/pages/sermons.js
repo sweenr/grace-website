ecb862152aba5fd9da57da183a675779383261d4
Richard Sween <sweenr418@gmail.com>
2020-04-13 19:03:39 -0500

    add pages

---
+++ src/pages/sermons.js
@@ -0,0 +1,59 @@
+import React from "react"
+
+function Sermons({ data }) {
+  const sermons = data.allMarkdownRemark.edges
+  return (
+    <>
+      <div className="content">
+        <div className="info-block">
+          <h2>Sermons</h2>
+          {sermons.length === 0 ? (
+            <p>There are currently no sermons to share.</p>
+          ) : (
+            ""
+          )}
+          {sermons.map(({ node }) => {
+            return (
+              <div className="sermon-listing">
+                <a href={`/sermons${node.fields.slug}`}>
+                  <h3>{node.frontmatter.title}</h3>
+                </a>
+                <small>{node.frontmatter.date}</small>
+                <p>{node.excerpt}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Sermons
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { templatePath: { eq: "sermon-template.js" } } }
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
