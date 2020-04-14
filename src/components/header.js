17d6802a1a5e40763c6bf23bd95c1cd525b17eaa
Richard Sween <sweenr418@gmail.com>
2020-04-13 20:52:37 -0500

    fix bug in data loading in header

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 import Img from "gatsby-image"
 
-function Header() {
+function Header({ data }) {
   // if (typeof document !== `undefined`) {
-  const toggleMenu = ({ data }) => {
+  const toggleMenu = () => {
     var x = document.getElementById("menuList")
     if (x.className === "menu") {
       x.className += " responsive"
