1dc625779fd306803154487fb6580ede1b9e8534
Richard Sween <sweenr418@gmail.com>
2022-05-11 20:29:51 -0500

    Outreach page changes to match CMS

--- src/markdown-pages/outreach.md
+++ src/markdown-pages/outreach.md
@@ -1,31 +1,26 @@
 ---
 path: /outreach
+ministries:
+  - summary: |-
+      Something about Prayer Shawls here
+    title: Prayer Shawl Ministry
+  - summary: |-
+      Something about food pantry here
+    title: Food Pantry
+  - summary: |-
+      Something about Good Samaritan here
+    title: Good Samaritan
+  - summary: |-
+      Something about VBS here
+    title: Vacation Bible School
+  - summary: |-
+      Something about VA outreach here
+    title: VA Ministry
+  - summary: |-
+      Something about senior breakfast here
+    title: Senior Breakfast
 ---
 
 ## Outreach Ministries of Grace Lutheran Church
 
 We're blessed to be able to support a variety of ministries in our community.
-
-### Prayer Shawl Ministry
-
-Something about Prayer Shawls here
-
-### Food Pantry
-
-Something about food pantry here
-
-### Good Samaritan
-
-Something about Good Samaritan here
-
-### Vacation Bible School
-
-Something about VBS here
-
-### VA Ministry
-
-Something about VA outreach here
-
-### Senior Breakfast
-
-Something about senior breakfast here
--- src/pages/outreach.js
+++ src/pages/outreach.js
@@ -1,18 +1,30 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
+import ReactMarkdown from "react-markdown"
 
 const Outreach = ({
   data: {
-    markdownRemark: { html },
+    markdownRemark: {
+      frontmatter: { ministries },
+      html,
+    },
   },
 }) => (
   <>
     <Helmet>
       <title>Outreach</title>
     </Helmet>
-    <div className="content">
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+    <div className="content outreach-content">
+      <div className="heading" dangerouslySetInnerHTML={{ __html: html }} />
+      {ministries.map(({ title, summary }) => {
+        return (
+          <section className="ministry">
+            <h3>{title}</h3>
+            <ReactMarkdown children={summary} />
+          </section>
+        )
+      })}
     </div>
   </>
 )
@@ -20,8 +32,14 @@ const Outreach = ({
 export default Outreach
 
 export const pageQuery = graphql`
-  query OutreachQuery {
+  query Outreach {
     markdownRemark(frontmatter: { path: { eq: "/outreach" } }) {
+      frontmatter {
+        ministries {
+          title
+          summary
+        }
+      }
       html
     }
   }
