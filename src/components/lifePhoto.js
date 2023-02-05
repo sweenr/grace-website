b1eb8babf78e9b86504c8ac3030083a5e5208dd3
Richard Sween <sweenr418@gmail.com>
2023-02-05 11:58:18 -0600

    First pass at image descriptions

--- src/components/lifePhoto.js
+++ src/components/lifePhoto.js
@@ -5,6 +5,7 @@ export const LifePhoto = ({ image, alt, description }) => {
   return (
     <div className="photo">
       <GatsbyImage image={image} alt={alt} />
+      <div className="desc">{alt}</div>
     </div>
   )
 }
